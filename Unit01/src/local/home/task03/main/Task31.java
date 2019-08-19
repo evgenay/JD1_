3d7ab15b05cc4b8eec17c2fd92d75c6a9c716220
evgenay <54099376+evgenay@users.noreply.github.com>
2019-08-19 23:42:49 +0300

    Add files via upload

---
+++ Unit01/src/local/home/task03/main/Task31.java
@@ -0,0 +1,46 @@
+package local.home.task03.main;
+
+import java.util.Scanner;
+
+/* Компьютер генерирует пять чисел в диапазоне от 1 до 15 включительно. Человек пытается их
+ *угадать. Программа должна выводить угаданные и неугаданные числа из тех, что сгенерировала
+ *программа, а также ошибочные числа пользователя.
+ */
+
+public class Task31 {
+	public static void main(String[] args) {
+		double a = 1;
+		double b;
+		double c = 1;
+
+		do {
+			a = Math.random() * 15;
+			a = (int) a;
+
+			System.out.println(a);
+
+			@SuppressWarnings("resource")
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Введите числа от 1 до 15 ");
+			System.out.print("> ");
+
+			while (!sc.hasNextInt()) {
+				sc.next();
+				System.out.print("Неверное значение, повторите ещё раз>  ");
+			}
+
+			b = sc.nextInt();
+
+			if (a == b) {
+				System.out.println("Угаданная цифра " + b);
+			} else if (a != b) {
+				System.out.println("Цифру вы не угадали -  " + b);
+				System.out.println("Цифра ПК - " + a);
+
+			}
+			c++;
+		} while (c <= 5);
+		System.out.println("---Конец игры--- ");
+
+	}
+}
