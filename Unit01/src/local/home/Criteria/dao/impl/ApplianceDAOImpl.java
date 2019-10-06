39d3742d9291bbe67bd35bf8e381d58b003321f5
evgenay <54099376+evgenay@users.noreply.github.com>
2019-10-07 00:06:08 +0300

    Update ApplianceDAOImpl.java

--- Unit01/src/local/home/Criteria/dao/impl/ApplianceDAOImpl.java
+++ Unit01/src/local/home/Criteria/dao/impl/ApplianceDAOImpl.java
@@ -3,22 +3,77 @@ package by.tc.task01.dao.impl;
 import by.tc.task01.dao.ApplianceDAO;
 import by.tc.task01.entity.Appliance;
 import by.tc.task01.entity.criteria.Criteria;
+import by.tc.task01.dao.ApplianceDAO;
+import by.tc.task01.dao.DAOException;
+import by.tc.task01.entity.Appliance;
+import by.tc.task01.entity.criteria.Criteria;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ApplianceDAOImpl implements ApplianceDAO{
 
-	@Override
-	public <E> Appliance find(Criteria<E> criteria) {
-		String categoryOfGoods;	
-		
-		categoryOfGoods = criteria.getGroupSearchName().getSimpleName();//"Oven"
-		
-		System.out.println("---" + categoryOfGoods);
-		// you may add your own code here
-		return null;
+	 private static final String FILE = "resources\\appliances_db.txt";
+
+    @Override
+    public List<Appliance> find(Criteria criteria) throws DAOException {
+
+	List<Appliance> findAppliance = new ArrayList<Appliance>();
+	String groupSearchName = criteria.getGroupSearchName();
+	Map<String, Object> params;
+
+	try {
+	    BufferedReader reader = new BufferedReader(new FileReader(FILE));
+
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+		String applianceType = line.split(" ")[0];
+
+		if (groupSearchName.equals(applianceType)) {
+		    params = getCriteria(line);
+
+		    if (compareCriteria(params, criteria.getCriteria())) {
+			findAppliance.add(ApplianceFactory.newAppliance(params));
+		    }
+		}
+	    }
+	    
+	    reader.close();
+	} catch (IOException e) {
+	    throw new DAOException(e);
+	}
+	return findAppliance;
+    }
+
+    private Map<String, Object> getCriteria(String line) {
+	Map<String, Object> criteria = new HashMap<String, Object>();
+	String[] params = line.split("\\s?[,:;]?\\s+");
+
+	criteria.put("Appliance", params[0]);
+
+	for (int i = 1; i < params.length; i++) {
+	    criteria.put(params[i].split("=")[0], params[i].split("=")[1]);
 	}
 	
-	// you may add your own code here
+	return criteria;
+    }
 
+    private boolean compareCriteria(Map<String, Object> parametres, Map<String, Object> criteria) {
+	boolean isParam = true;
+
+	for (Map.Entry<String, Object> entry : criteria.entrySet()) {
+	    String st = entry.getKey();
+	    if (!(criteria.get(st).toString()).equals(parametres.get(st))) {
+		isParam = false;
+		break;
+	    }
+	}
+	return isParam;
+    }
 }
 
 
