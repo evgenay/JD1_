76daebebcbede4033294e1c9a124a99a7e8866c9
evgenay <54099376+evgenay@users.noreply.github.com>
2019-10-06 23:59:33 +0300

    Update ApplianceService.java

--- Unit01/src/local/home/Criteria/service/ApplianceService.java
+++ Unit01/src/local/home/Criteria/service/ApplianceService.java
@@ -2,6 +2,13 @@ package by.tc.task01.service;
 
 import by.tc.task01.entity.Appliance;
 import by.tc.task01.entity.criteria.Criteria;
+import by.tc.task01.dao.ApplianceDAO;
+import by.tc.task01.dao.DAOException;
+import by.tc.task01.dao.DAOFactory;
+import by.tc.task01.entity.Appliance;
+import by.tc.task01.entity.criteria.Criteria;
+import by.tc.task01.service.ApplianceService;
+import by.tc.task01.service.validation.Validator;
 
 public interface ApplianceService {	
 	
