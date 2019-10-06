e3a2f52e6088bd6e451bce0ecef20dffa65d2a4b
evgenay <54099376+evgenay@users.noreply.github.com>
2019-10-06 23:47:35 +0300

    Update Validator.java

--- Unit01/src/local/home/Criteria/service/validation/Validator.java
+++ Unit01/src/local/home/Criteria/service/validation/Validator.java
@@ -5,11 +5,47 @@ import by.tc.task01.entity.criteria.Criteria;
 public class Validator {
 	
 	public static <E> boolean criteriaValidator(Criteria<E> criteria) {
-		// you may add your own code here
-		
-		return true;
-	}
+		String product = criteria.getGroupSearchName();
 
+	switch (product) {
+	case "Oven":
+	    return validateOven(criteria);
+	case "VacuumCleaner":
+	    return true;
+	case "Laptop":
+	    return true;
+	case "Speakers":
+	    return true;
+	case "Refrigerator":
+	    return true;
+	case "TabletPC":
+	    return true;
+	default:
+	    return false;
+	}
+    }
+
+    private static boolean validateOven(Criteria criteria) {
+	Map<String, String> ovenParams = new HashMap<>();
+	ovenParams.put(Oven.HEIGHT.toString(), "[0-9]{2,3}\\.[0-9]");
+	ovenParams.put(Oven.WIDTH.toString(), "[0-9]{2,3}");   
+	ovenParams.put(Oven.POWER_CONSUMPTION.toString(), "[0-9]{3,4}");
+	ovenParams.put(Oven.WEIGHT.toString(), "[0-9]{2,3}");
+	ovenParams.put(Oven.CAPACITY.toString(), "[0-9]{2,3}");
+	ovenParams.put(Oven.DEPTH.toString(), "[0-9]{2,3}");
+	
+
+	Set<String> keys = criteria.getCriteria().keySet();
+	for (String key : keys) {
+	    String regex = ovenParams.get(key);
+	    String params = criteria.getCriteria().get(key) + "";
+
+	    if (!Pattern.matches(regex, params)) {
+		return false;
+	    }
+	}
+	return true;
+    }
 }
 
 //you may add your own new classes
