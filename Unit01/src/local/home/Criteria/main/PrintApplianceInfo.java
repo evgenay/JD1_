d1960fa3e3ae25ff778aed73100a5f6baf5e6919
evgenay <54099376+evgenay@users.noreply.github.com>
2019-10-08 02:06:07 +0300

    Update PrintApplianceInfo.java

--- Unit01/src/local/home/Criteria/main/PrintApplianceInfo.java
+++ Unit01/src/local/home/Criteria/main/PrintApplianceInfo.java
@@ -4,11 +4,14 @@ import by.tc.task01.entity.Appliance;
 
 public class PrintApplianceInfo {
 	
-	public static void print(Appliance appliance) {
-		// you may add your own code here
-		
+	public static void print(List<Appliance> appliances) {
+		if (appliances == null) {
+			System.out.println("По данным критериям товар не найден ");
+		} else {
+			for (Appliance appliance : appliances) {
+				System.out.println(appliance.toString());
+			}
+		}
+		System.out.println("---------------------------------");
 	}
-	
-	// you may add your own code here
-
 }
