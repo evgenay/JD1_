388e351507c1cc45e9b675a4213819c6c9ff9ef2
evgenay <54099376+evgenay@users.noreply.github.com>
2019-10-08 02:04:13 +0300

    Update Validator.java

--- Unit01/src/local/home/Criteria/service/validation/Validator.java
+++ Unit01/src/local/home/Criteria/service/validation/Validator.java
@@ -8,26 +8,47 @@ import by.tc.task01.entity.criteria.SearchCriteria.Refrigerator;
 import by.tc.task01.java.util.regex.Pattern;
 
 public class Validator {
-	
-	public static <E> boolean criteriaValidator(Criteria<E> criteria) {
+
+	public static boolean criteriaValidator(Criteria criteria) {
 		String product = criteria.getGroupSearchName();
 
-	switch (product) {
-	case "Oven":
-	    return true;
-	case "VacuumCleaner":
-	    return true;
-	case "Laptop":
-	    return true;
-	case "Speakers":
-	    return true;
-	case "Refrigerator":
-            return validateRefrigerator(criteria);
-	case "TabletPC":
-	    return true;
-	default:
-	    return false;
+		switch (product) {
+		case "Oven":
+			return true;
+		case "Laptop":
+			return true;
+		case "Refrigerator":
+			return validateRefrigerator(criteria);
+		case "VacuumCleaner":
+			return true;
+		case "TabletPC":
+			return true;
+		case "Speakers":
+			return true;
+		default:
+			return false;
+		}
 	}
-  }
 
-//you may add your own new classes
+	private static boolean validateRefrigerator(Criteria criteria) {
+		Map<String, String> refrParam = new HashMap<>();
+		refrParam.put(Refrigerator.POWER_CONSUMPTION.toString(), "[0-9]{3,4}");
+		refrParam.put(Refrigerator.WEIGHT.toString(), "[0-9]{2,3}");
+		refrParam.put(Refrigerator.FREEZER_CAPACITY.toString(), "[0-9]{2,3}");
+		refrParam.put(Refrigerator.OVERALL_CAPACITY.toString(), "[0-9]{2,3}");
+		refrParam.put(Refrigerator.HEIGHT.toString(), "[0-9]{2,3}\\.[0-9]");
+		refrParam.put(Refrigerator.WIDTH.toString(), "[0-9]{2,3}");
+
+		Set<String> criteriakeys = criteria.getCriteria().keySet();
+		for (String key : criteriakeys) {
+			String regex = refrParam.get(key);
+			String input = criteria.getCriteria().get(key)+"";
+
+			if (!Pattern.matches(regex, input)) {
+				return false;
+			}
+		}
+		return true;
+	
+	}
+}
